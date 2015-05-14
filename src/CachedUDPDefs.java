6a16c9fd3d295d8f25a0e3ce75b207bcf3bd8c77
xlrtx <gho.xlrtx@gmail.com>
2015-05-15 03:28:10 +1000

    First commit, still implementing.
    Client is not complete yet.

---
+++ src/CachedUDPDefs.java
@@ -0,0 +1,17 @@
+
+public interface CachedUDPDefs {
+  
+  
+  // Server Time Out
+  public static final int SERVER_TIME_OUT   = 3000;
+  // Server Cache Time Out
+  public static final int SERVER_CACHE_TTL  = 10000;
+  // Client Time Out
+  public static final int CLIENT_TIME_OUT   = 3000;
+  // Data package size
+  public static final int MAX_PACKET_SIZE   = 1024;
+  
+ 
+  
+  
+}
